148183cfe7704662c43ed5ff638c196a41015c0f
Lokesh Matta <iamlokeshofficial@gmail.com>
2024-04-07 16:46:55 +0530

    certifications ui updated

--- index.js
+++ index.js
@@ -1,37 +1,52 @@
-const projects = [
-  {
-    title: "Share Your Notes",
-    info: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse odio sequi provident quidem tenetur aliquid dolor fuga,consequuntur autem dolore dignissimos, quae quaerat ipsa sunt hicharum tempora, iste nesciunt?",
-  },
-  {
-    title: "Share Your Notes",
-    info: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse odio sequi provident quidem tenetur aliquid dolor fuga,consequuntur autem dolore dignissimos, quae quaerat ipsa sunt hicharum tempora, iste nesciunt?",
-  },
-  {
-    title: "Share Your Notes",
-    info: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse odio sequi provident quidem tenetur aliquid dolor fuga,consequuntur autem dolore dignissimos, quae quaerat ipsa sunt hicharum tempora, iste nesciunt?",
-  },
-];
-
-const pro_container = document.querySelector(".pro-container");
-
-const project = projects.map(
-  (project) => `
-    <div class="pro-card">
-      <p class="pro-title">${project.title}</p>
-      <p class="pro-info">${project.info}</p>
-      <button>Github</button>
-    </div>`
-);
-
-// pro_container.innerHTML = project;
-
-// const outputs = document.querySelector("#out");
-
 // Css Toogle
 const impDiv = document.querySelector("#imp");
 const btn = document.querySelector("#imp-btn");
 
 btn.addEventListener("click", () => {
   impDiv.classList.add("hide");
+});
+
+// Certifications
+
+const certs = [
+  {
+    title: "AWS Certified Cloud Practitioner",
+    info: "hello",
+    img: "images/Certificates/AWSCP.png",
+  },
+  {
+    title: "AWS Certified Solutions Architect",
+    info: "hello 3",
+    img: "images/Certificates/AWSSAA.png",
+  },
+  {
+    title: "Red Hat Certified Engineer",
+    info: "hello",
+    img: "images/Certificates/Redhat.png",
+  },
+];
+
+const certTitle = document.querySelector(".cert-title");
+const certInfo = document.querySelector(".cert-info");
+const certImg = document.querySelector("#cert-img");
+
+const prevCert = document.querySelector("#prevC");
+const nextCert = document.querySelector("#nextC");
+
+let num = 0;
+
+nextCert.addEventListener("click", () => {
+  num++;
+  certTitle.innerHTML = certs[num].title;
+  certInfo.innerHTML = certs[num].info;
+  certImg.src = certs[num].img;
+});
+
+prevCert.addEventListener("click", () => {
+  if (num != 0) {
+    num--;
+    certTitle.innerHTML = certs[num].title;
+    certInfo.innerHTML = certs[num].info;
+    certImg.src = certs[num].img;
+  }
 });
